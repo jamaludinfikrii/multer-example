c0004223f1f434c5c82cf5472a7f45c774af7a60
fikri <me@fikri.tech>
2020-04-21 11:48:01 +0700

    initial commit

---
+++ precache-manifest.6e3b39164d332085cacbd67e0d3a11e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42054991d0c231ec3bdb5e02289487c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b1910fd7b02a090ceb4",
+    "url": "/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "37d335bf026684618ba2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b1910fd7b02a090ceb4",
+    "url": "/static/js/2.4f74d21d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4f74d21d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d335bf026684618ba2",
+    "url": "/static/js/main.05a21285.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
